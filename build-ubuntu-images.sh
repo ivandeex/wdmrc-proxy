97255ba33034be43a894317635b8ee2cff541ae8
Ivan Andreev <ivandeex@gmail.com>
2019-06-19 12:59:16 +0300

    upgrade to latest wdmrc 1.10.7.10, keep only xenial/mono

--- build-ubuntu-images.sh
+++ build-ubuntu-images.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-WDMRC_VERS="1.10.2.2"
+WDMRC_VERS="1.10.7.10"
+ALL_IMAGES="xenial-core xenial-mono bionic-core bionic-mono"
+BLD_IMAGES="xenial-mono"
 
-for DIR in xenial-core xenial-mono bionic-core bionic-mono ; do
+for DIR in $BLD_IMAGES ; do
   perl -i -p -e "s/WDMRC_VERS=\"[0-9.]+\"/WDMRC_VERS=\"$WDMRC_VERS\"/g" $DIR/Dockerfile
 done
 
 #exit
 
-for DIR in xenial-core xenial-mono bionic-core bionic-mono ; do
+for DIR in $BLD_IMAGES ; do
   TAG="ivandeex/wdmrc:v$WDMRC_VERS-${DIR/-/.}"
   echo ">>>>>>>>>>>>>>>> BUILDING: $TAG ..."
   echo
